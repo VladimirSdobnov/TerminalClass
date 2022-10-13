5ad842347c8e116df01afaa6f9e85c21ec34e829
VladimirSdobniv <itmm-2213-a@E.local>
2022-10-13 09:42:15 +0300

    Dificult mystake

--- Terminalclass/Stroki/Stroki.c
+++ Terminalclass/Stroki/Stroki.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 
 int main() {
+	char* str;
+	int size;
 
+	printf("Enter str length: ");
+	scanf_s("%d", &size);
+
+	str = (char*)malloc(sizeof(char) * size);
+	
+	gets(str);//принимает строку
+	puts(str);// выводит строчку
+	printf("%s", str);
+
+
+	free(str);
+	return 0;
 }
