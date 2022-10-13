ebac489529cce6f0685b764c03d0434d8a08a53e
VladimirSdobniv <itmm-2213-a@E.local>
2022-10-13 10:32:17 +0300

    Seriosly work

--- Terminalclass/Stroki/Stroki.c
+++ Terminalclass/Stroki/Stroki.c
@@ -2,30 +2,52 @@
 #include <stdlib.h>
 #include <malloc.h>
 
+void catch (char* str, int size) {
+	str[size] = '\0';
+	printf("New str <%s>", str);
+}
+
 int main() {
 	char* str;
-	int size, realsize = 0;
-
-	printf("Enter str length: ");
-	scanf_s("%d", &size);
-
-	str = (char*)malloc(sizeof(char) * size);
-
+	char old, new;
+	int size, realsize = 0, want = 1;
+	str = (char*)malloc(sizeof(char) * 100);
 	printf("Enter str: ");
-	getchar();
-	gets(str);//принимает строку
-	puts(str);// выводит строчку
-	printf("%s", str);
+	gets(str);
+	printf("What are you want to do?(1 - enter new str, 2 - catch str, 3 - remote element, 4 - change registr): ");
+	scanf_s("%d", &want);
+
+	while (want == 1) {
+		printf("Enter str: ");
+		getchar();
+		gets(str);
+
+		printf("What are you want to do?(1 - enter new str, 2 - catch str, 3 - remote element, 4 - change registr): ");
+		scanf_s("%d", &want);
+	}
 
 	while (str[realsize] != '\0') {
 		realsize += 1;
 	}
 
-	printf("\nYou enter str <%s> and it's length %d\n", str, realsize);
+	if (want == 2) {
+		printf("Enter size of catch: ");
+		scanf_s("%d", &size);
+		while (size > realsize || size < 0) {
+			printf("Very strange catch, try again: ");
+			scanf_s("%d", &size);
+		}
+		catch (str, size);
+	}
+	else if (want == 3) {
+		getchar();
+		printf("Enter element wich you want remote: ");
+		old = getchar();
 
-	printf("and now we deleated all str witout first element)\n");
-	str[1] = '\0';
-	printf("%s", str);
+		getchar();
+		printf("Enter new element: ");
+		new = getchar();
+	}
 
 	free(str);
 	return 0;
