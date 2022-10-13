0a70c008ff2cfd655c55d260bfe262705fd81991
VladimirSdobniv <itmm-2213-a@E.local>
2022-10-13 09:52:13 +0300

    It's realy workgit add Terminalclass!

--- Terminalclass/Stroki/Stroki.c
+++ Terminalclass/Stroki/Stroki.c
@@ -4,17 +4,24 @@
 
 int main() {
 	char* str;
-	int size;
+	int size, realsize = 0;
 
 	printf("Enter str length: ");
 	scanf_s("%d", &size);
 
 	str = (char*)malloc(sizeof(char) * size);
-	
+
+	printf("Enter str: ");
+	getchar();
 	gets(str);//принимает строку
 	puts(str);// выводит строчку
 	printf("%s", str);
 
+	while (str[realsize] != '\0') {
+		realsize += 1;
+	}
+
+	printf("\nYou enter str <%s> and it's length %d", str, realsize);
 
 	free(str);
 	return 0;
