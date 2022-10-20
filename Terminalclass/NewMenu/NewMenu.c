98e7a19ff1a7c6621fbc53e229cfeb012bde9b7b
VladimirSdobniv <itmm-2213-a@E.local>
2022-10-20 08:55:19 +0300

    Добавлена классная работа и пример меню

--- Terminalclass/NewMenu/NewMenu.c
+++ Terminalclass/NewMenu/NewMenu.c
@@ -3,6 +3,9 @@
 #include <locale.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <malloc.h>
 
 // управл¤ющие консолью библиотеки
 #include <windows.h>
@@ -23,42 +26,54 @@
 #define KEY_ARROW_UP 72
 #define KEY_ARROW_DOWN 80
 
+void generate(int* mass, int size) {
+    for (int i = 0; i < size; i++) {
+        scanf_s("%d", &mass[i]);
+    }
+}
+void show(int* mass, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", mass[i]);
+    }
+}
 // объ¤вление главной функции
 int main() {
     // настройки
-
-    // задаЄм название консольного окна
+    setlocale(LC_ALL, "rus");
+    srand(time(0));
+    // задаем название консольного окна
     system("title Application");
 
     // устанавливаем взаимодействие с русскими буквами
     SetConsoleCP(1251);         // установка кодовой страницы win-cp 1251 в поток ввода
     SetConsoleOutputCP(1251);   // установка кодовой страницы win-cp 1251 в поток вывода
 
-    // получаем дескриптор окна дл¤ обращени¤ к консоли
+    // получаем дескриптор окна для обращени¤ к консоли
     HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    // отключение мигани¤ курсора в консоли (в нашем меню курсор лишний)
+    // отключение мигания курсора в консоли (в нашем меню курсор лишний)
     CONSOLE_CURSOR_INFO structCursorInfo;
     GetConsoleCursorInfo(hStdOut, &structCursorInfo);
     structCursorInfo.bVisible = FALSE;
     SetConsoleCursorInfo(hStdOut, &structCursorInfo);
 
-    // задаЄм серый фон дл¤ окна консоли
+    // задаем серый фон дл¤ окна консоли
     SetConsoleTextAttribute(hStdOut, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE);
     // очищаем экран, чтобы настройка цвета применилась
     system("cls");
 
     // объ¤вление переменных
-    int choose_pos;                    // переменна¤ дл¤ хранени¤ выбранного пункта меню
-    int iKey;                          // переменна¤ дл¤ фиксации нажатой клавиши
-    int exit_flag;                     // флаг на завершение программы
-    COORD cursorPos;                   // управление положением курсора, чтобы мен¤ть выводимый текст
+    int* mass = {0};
+    int size = 0;
+    int choose_pos = 0;                    // переменная для хранения выбранного пункта меню
+    int iKey;                          // переменная для фиксации нажатой клавиши
+    int exit_flag = 0;                     // флаг на завершение программы
+    int return_flag = 0;
+    COORD cursorPos;                   // управление положением курсора, чтобы менять выводимый текст
 
-    char* menu[MENU_SIZE] = { "Start", "Settings", "Exit" };
-
-    // инициализаци¤ (заполнение значени¤ми) переменных
-    exit_flag = 0;
-    choose_pos = 0;
+    char* menu_start[MENU_SIZE] = { "Алгоритмы", "Настройки", "Выход" };
+    char* menu_algor[MENU_SIZE] = { "Поиск", "Сортировка", "Назад" };
+    char* menu_setting[MENU_SIZE] = { "Задать массив", "Посмотреть текущий массив", "Назад"};
 
     while (!exit_flag) {
         /* 1. вывод меню с переключением */
@@ -69,8 +84,9 @@ int main() {
         cursorPos = (COORD){ 0, 0 };
         SetConsoleCursorPosition(hStdOut, cursorPos);
 
-        // контроль нажати¤ клавиш
+        // контроль нажатия клавиш
         while (iKey != KEY_EXIT && iKey != KEY_ENTER) {
+            return_flag = 0;
             switch (iKey) {
             case KEY_ARROW_UP:
                 choose_pos--;
@@ -82,21 +98,21 @@ int main() {
 
             system("cls");
 
-            // зацикливание перелистывани¤ меню
+            // зацикливание перелистывания меню
             if (choose_pos < 0) { choose_pos = MENU_SIZE - 1; }
             if (choose_pos > MENU_SIZE - 1) { choose_pos = 0; }
 
             for (int i = 0; i < MENU_SIZE; i++) {
                 cursorPos = (COORD){ 3, i };
                 SetConsoleCursorPosition(hStdOut, cursorPos);
-                printf("%s \n", menu[i]);
+                printf("%s \n", menu_start[i]);
             }
 
             // обозначение выбранной позиции
             cursorPos = (COORD){ 0, choose_pos };
             SetConsoleCursorPosition(hStdOut, cursorPos);
             printf("<<", iKey);
-            cursorPos = (COORD){ strlen(menu[choose_pos]) + 3 + 1, choose_pos };
+            cursorPos = (COORD){ strlen(menu_start[choose_pos]) + 3 + 1, choose_pos };
             SetConsoleCursorPosition(hStdOut, cursorPos);
             printf(">>");
 
@@ -113,8 +129,75 @@ int main() {
             break;
         case 1:
             system("cls");
-            printf("Settings part in development\n");
-            system("pause");
+            choose_pos = 0;
+            while (!return_flag) {
+                iKey = 67;
+                cursorPos = (COORD){ 0, 0 };
+                SetConsoleCursorPosition(hStdOut, cursorPos);
+
+                while (iKey != KEY_EXIT && iKey != KEY_ENTER) {
+                    switch (iKey) {
+                    case KEY_ARROW_UP:
+                        choose_pos--;
+                        break;
+                    case KEY_ARROW_DOWN:
+                        choose_pos++;
+                        break;
+                    }
+
+                    system("cls");
+
+                    if (choose_pos < 0) { choose_pos = MENU_SIZE - 1; }
+                    if (choose_pos > MENU_SIZE - 1) { choose_pos = 0; }
+
+                    for (int i = 0; i < MENU_SIZE; i++) {
+                        cursorPos = (COORD){ 3, i };
+                        SetConsoleCursorPosition(hStdOut, cursorPos);
+                        printf("%s \n", menu_setting[i]);
+                    }
+
+                    cursorPos = (COORD){ 0, choose_pos };
+                    SetConsoleCursorPosition(hStdOut, cursorPos);
+                    printf("<<", iKey);
+                    cursorPos = (COORD){ strlen(menu_setting[choose_pos]) + 3 + 1, choose_pos };
+                    SetConsoleCursorPosition(hStdOut, cursorPos);
+                    printf(">>");
+
+                    iKey = _getch();
+                }
+                switch (choose_pos) {
+                case 0:
+                    system("cls");
+                    printf("Введите размер массива: ");
+                    scanf_s("%d", &size);
+                    system("cls");
+                    mass = (int*)malloc(sizeof(int) * size);
+                    printf("Введите элементы массива: ");
+                    generate(mass, size);
+                    system("cls");
+                    printf("Массив Сгенерирован!\n");
+                    system("pause");
+                    break;
+                case 1:
+                    system("cls");
+                    if (size == 0) {
+                        printf("Массив еще не задан!\n");
+                        system("pause");
+                        break;
+                    }
+                    else {
+                        system("cls");
+                        printf("Текущий массив: ");
+                        show(mass, size);
+                        printf("\n");
+                        system("pause");
+                        break;
+                    }
+                case 2:
+                    return_flag = 1;
+                    break;
+                }
+            }
             break;
         case 2:
             exit_flag = 1;
